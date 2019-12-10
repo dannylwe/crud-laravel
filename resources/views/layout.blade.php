bb3991964e733d2a9a3de180de48c11f781379f1
Daniel Lwetabe <acehanks@gmail.com>
2019-12-10 12:59:40 +0300

    add authentication, add create route, add vue scaffolding and bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laravel 5.8 CRUD Example Tutorial</title>
-  <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
   <div class="container">
